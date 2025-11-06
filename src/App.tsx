66dbe36bbb8bf7a3304833c90f94112eec9f3ada
gabriel degobi <gwillians98@gmail.com>
2025-11-06 17:33:07 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+//Components
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header/>
+        <h1>Conteúdo</h1>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
